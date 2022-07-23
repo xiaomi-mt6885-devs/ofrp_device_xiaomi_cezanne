007eb691b9067293ebad64912f8feb57dfa197e3
AngelaCooljx <ljx15007902367@gmail.com>
2022-07-23 13:00:46 +0800

    cezanne: Fixed DATA decrypt on Android12

--- device.mk
+++ device.mk
@@ -9,27 +9,20 @@ LOCAL_PATH := device/xiaomi/cezanne
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+# Fastbootd
+TW_INCLUDE_FASTBOOTD := true
 
 # API
 PRODUCT_TARGET_VNDK_VERSION := 31
 PRODUCT_SHIPPING_API_LEVEL := 29
 
-# Fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery \
-    fastbootd
-
 # Additional binaries & libraries needed for recovery
 TARGET_RECOVERY_DEVICE_MODULES += \
     libkeymaster41 \
     libkeymaster4 \
-    libpuresoftkeymasterdevice \
-    libandroidicu \
-    libion
+    libpuresoftkeymasterdevice
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
